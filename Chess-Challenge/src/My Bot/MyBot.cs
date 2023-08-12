8aabcaef3982372e4d1358f0847970188b6fc251
Nicolas Reyland <nicolas@reyland.dev>
2023-08-12 13:36:01 +0200

    dummy minimax bot

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,68 @@
-﻿using ChessChallenge.API;
+using ChessChallenge.API;
+using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
 
 public class MyBot : IChessBot
 {
+    // Piece values: null, pawn, knight, bishop, rook, queen, king
+    readonly int[] pieceValues = { 0, 10, 30, 30, 50, 90, 1000 };
+    int searchDepth = 6;
+    Move moveToPlay;
+
     public Move Think(Board board, Timer timer)
     {
+        searchDepth = chooseSearchDepth(board);
+        moveToPlay = Move.NullMove;
+        int score = Search(board, -int.MaxValue, int.MaxValue, searchDepth);
+        return moveToPlay;
+    }
+
+    private int chooseSearchDepth(Board board) {
+        return 6;
+    }
+
+    private int Evaluate(Board board)
+    {
+        int boardValue = 0;
+        PieceList[] allPieces = board.GetAllPieceLists();
+        foreach (PieceList Pieces in allPieces){
+            int value = Pieces.Count * pieceValues[(int)Pieces.TypeOfPieceInList];
+            boardValue += board.IsWhiteToMove == Pieces.IsWhitePieceList ? value : -value;
+        }
+        return boardValue;
+    }
+
+    int Search (Board board, int alpha, int beta, int depth){
+        int bestScore = -int.MaxValue;
+        Move bestMove = Move.NullMove;
+
+        if (depth == 0) {
+            bestScore = Evaluate(board);
+            if (bestScore >= beta)
+                return bestScore;
+            alpha = Math.Max(alpha, bestScore);
+            return alpha;
+        }
+
         Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        for (int i=0; i<moves.Length; i++) {
+            board.MakeMove(moves[i]);
+            int score = -Search(board, -beta, -alpha, depth - 1);
+            board.UndoMove(moves[i]);
+            if (score > bestScore) {
+                bestScore = score;
+                bestMove = moves[i];
+                alpha = Math.Max(alpha, score);
+                if (alpha >= beta)
+                    break;
+            }
+        }
+        if (moves.Length == 0)
+            return board.IsInCheck() ? -pieceValues[6] + (searchDepth - depth) : 0;
+        if (depth == searchDepth)
+            moveToPlay = bestMove;
+        return bestScore;
     }
 }
